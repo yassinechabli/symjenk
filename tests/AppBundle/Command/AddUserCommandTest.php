3ef0d657b69a6a2c92e8f2494e6069e5b058c09b
yassinechabli <yassine.chabli@edu.umi.ac.ma>
2017-04-14 19:40:24 +0200

    test pro

--- tests/AppBundle/Command/AddUserCommandTest.php
+++ tests/AppBundle/Command/AddUserCommandTest.php
@@ -43,26 +43,7 @@ class AddUserCommandTest extends KernelTestCase
         $this->assertUserCreated($isAdmin);
     }
 
-    /**
-     * @dataProvider isAdminDataProvider
-     *
-     * This test doesn't provide all the arguments required by the command, so
-     * the command runs interactively and it will ask for the value of the missing
-     * arguments.
-     * See https://symfony.com/doc/current/components/console/helpers/questionhelper.html#testing-a-command-that-expects-input
-     */
-    public function testCreateUserInteractive($isAdmin)
-    {
-        $this->executeCommand(
-            // these are the arguments (only 1 is passed, the rest are missing)
-            $isAdmin ? ['--admin' => 1] : [],
-            // these are the responses given to the questions asked by the command
-            // to get the value of the missing required arguments
-            array_values($this->userData)
-        );
-
-        $this->assertUserCreated($isAdmin);
-    }
+    
 
     /**
      * This is used to execute the same test twice: first for normal users
